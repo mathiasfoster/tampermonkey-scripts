dc9ffe675b0207773316627e30268175a6c9a7de
Mathias Foster <mathias.foster@outlook.com>
2020-12-01 08:44:32 +1300

    Update View past Unleashed customer orders.user.js
    Script was failing in December each year, because `monthNames[12]` was null.
    Added fix for this.

--- View past Unleashed customer orders.user.js
+++ View past Unleashed customer orders.user.js
@@ -12,18 +12,37 @@
     'use strict';
     console.log("tampermonkey active");
     let customerID = window.location.href.slice(window.location.href.lastIndexOf("/") + 1, window.location.href.length);
+    console.log("Customer ID", customerID)
     let customerCode = document.querySelector('#CustomerCodeDisplay').innerText;
+    console.log("Customer Code", customerCode);
     customerCode = customerCode.replace(" ", "+");
     let dayDate = new Date().getDate().toString();
     dayDate = dayDate.padStart(2, "0");
+    console.log("Day Date", dayDate);
     let monthDate = new Date().getMonth() + 1;
     monthDate = monthDate.toString().padStart(2, "0");
-    let yearDate = new Date().getFullYear();
+    console.log("Month Date", monthDate);
+    let yearDate = new Date().getFullYear().toString();
+    console.log("Year Date", yearDate);
     const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let monthFull = monthNames[new Date().getMonth()];
-    let lastYearMonthFull = monthNames[new Date().getMonth() + 1];
-    let reportURL = "https://au.unleashedsoftware.com/v2/Enquiry/UnitSalesEnquiry#customerId=" + customerID.toString() + ",start=" + lastYearMonthFull.toString() + "+" + (yearDate - 1).toString() + ",end=" + monthFull.toString() + "+" + yearDate.toString() + ",customerCode=" + customerCode + ",includingCredits=true";
-    console.log(reportURL);
+    console.log("Month Full", monthFull);
+    let lastYearMonthFull;
+    if(new Date().getMonth() + 1 === 12) {
+        lastYearMonthFull = monthNames[0];
+    } else {
+       lastYearMonthFull = monthNames[new Date().getMonth() + 1];
+    }
+    let lastYearNumber;
+    if(new Date().getMonth() + 1 === 12) {
+        lastYearNumber = yearDate;
+    } else {
+        lastYearNumber = yearDate - 1;
+    }
+    console.log("Indice", new Date().getMonth() + 1);
+    console.log("Last Year Month Full", lastYearMonthFull);
+    let reportURL = "https://au.unleashedsoftware.com/v2/Enquiry/UnitSalesEnquiry#customerId=" + customerID.toString() + ",start=" + lastYearMonthFull.toString() + "+" + lastYearNumber.toString() + ",end=" + monthFull.toString() + "+" + yearDate.toString() + ",customerCode=" + customerCode + ",includingCredits=true";
+    //console.log(reportURL);
     let buttonContainer = document.querySelector('.buttons');
     let orderButton = document.createElement('a');
     orderButton.classList.add('fm-button');
