343803115d1c132c5a549b3edce295ba54e2def9
Mathias Foster <mathias.foster@outlook.com>
2022-07-11 16:08:19 +1200

    try to add/remove fields based on value of input

--- outline-required-fields-on-an-unleashed-sales-order.user.js
+++ outline-required-fields-on-an-unleashed-sales-order.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Outline required fields on an Unleashed sales order
-// @namespace    https://gwg/mz
-// @version      0.2
+// @namespace    https://gwg.nz
+// @version      0.3
 // @description  Outline required fields on an Unleashed sales order
 // @author       You
 // @match        https://au.unleashedsoftware.com/v2/SalesOrder/Update/*
@@ -12,16 +12,39 @@
 (function() {
     'use strict';
     let redBorder = "3px solid rgb(225, 30, 57)";
-    if(document.querySelector("#CustomerRef").value === "") {
-        let referenceField = document.querySelector("#InvoiceHeaderTable > tbody > tr:nth-child(3)");
+    
+    function changeBorder(element, value) {
+        if(value === "") {
+            element.style.border = redBorder;
+        } else {
+            element.style.border = "";
+        }
+    }
+    
+    let referenceFieldValue = document.querySelector("#CustomerRef");
+    let referenceField = document.querySelector("#InvoiceHeaderTable > tbody > tr:nth-child(3)");
+    if(referenceFieldValue.value === "") {
         referenceField.style.border = redBorder;
+        referenceFieldValue.addEventListener("change", (event) => {
+            changeBorder(referenceField, referenceFieldValue.value);
+        });
     }
-    if(document.querySelector("#DeliveryMethodList").value === "") {
-        let deliveryMethod = document.querySelector("#InvoiceHeaderTable > tbody > tr:nth-child(11)");
+    
+    let deliveryMethodValue = document.querySelector("#DeliveryMethodList");
+    let deliveryMethod = document.querySelector("#InvoiceHeaderTable > tbody > tr:nth-child(11)");
+    if(deliveryMethodValue.value === "") {
         deliveryMethod.style.border = redBorder;
+        deliveryMethodValue.addEventListener("change", (event) => {
+            changeBorder(deliveryMethod, deliveryMethodValue.value);
+        });
     }
-    if(document.querySelector("#DeliveryStreetAddress").value === "") {
-        let addressFields = document.querySelector("#DeliveryDetailsTable > tbody");
+    
+    let addressFieldsValue = document.querySelector("#DeliveryStreetAddress");
+    let addressFields = document.querySelector("#DeliveryDetailsTable > tbody");
+    if(addressFieldsValue.value === "") {
         addressFields.style.border = redBorder;
+        addressFieldsValue.addEventListener("change", (event) => {
+            changeBorder(addressFields, addressFieldsValue.value);
+        });
     }
 })();
