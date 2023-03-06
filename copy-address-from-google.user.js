2a9a181594e2e148e96726bc32c00a9c5dd14ac2
Mathias Foster <mathias.foster@outlook.com>
2023-03-07 08:02:20 +1300

    update because Google changed

--- copy-address-from-google.user.js
+++ copy-address-from-google.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Copy address from Google
 // @namespace    http://gwg.nz/
-// @version      0.3
+// @version      0.4
 // @description  Copy an address from Google with one click, in the right formatting
 // @author       Mathias Foster
 // @match        https://www.google.com/search?q=*
@@ -12,24 +12,17 @@
 (function() {
     'use strict';
 
-    // exit if the search isn't for an address
-    if(!document.querySelector) {
-        return;
-    }
-
     // selectors for the first and second line of the address
-    let firstLine = document.querySelector('.desktop-title-content');
-    let secondLine = document.querySelector('.desktop-title-subcontent');
+    let placeToPutButton = document.querySelector('div > b > div > div > div + span');
 
     // create and position the copy button
     let copyButton = document.createElement('button');
-    //copyButton.setAttribute("onclick", "copy()");
     copyButton.innerText = "📁";
     copyButton.id = "copyButton";
-    firstLine.parentElement.appendChild(copyButton);
+    placeToPutButton.parentElement.appendChild(copyButton);
 
     // add the copying functionality to the page
     let script = document.createElement('script');
-    script.innerText = `let firstLine = document.querySelector('.desktop-title-content').innerText; let secondLine = document.querySelector('.desktop-title-subcontent').innerText; document.querySelector("#copyButton").addEventListener("click", copy); function copy() {  const mySmartTextarea = document.createElement('textarea'); mySmartTextarea.innerHTML = firstLine + "\\r\\n" + secondLine; document.body.appendChild(mySmartTextarea); mySmartTextarea.select(); document.execCommand('copy'); document.body.removeChild(mySmartTextarea); document.querySelector("#copyButton").innerText = "✅"}`;
+    script.innerText = `let firstLine = document.querySelector('div > b > div > div > div').innerText; let secondLine = document.querySelector('div > b > div > div > div + span').innerText; document.querySelector("#copyButton").addEventListener("click", copy); function copy() {  const mySmartTextarea = document.createElement('textarea'); mySmartTextarea.innerHTML = firstLine + "\\r\\n" + secondLine; document.body.appendChild(mySmartTextarea); mySmartTextarea.select(); document.execCommand('copy'); document.body.removeChild(mySmartTextarea); document.querySelector("#copyButton").innerText = "✅"}`;
     document.body.appendChild(script);
 })();
