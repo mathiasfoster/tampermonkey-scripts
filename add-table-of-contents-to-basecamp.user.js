5cb2c03f9bca024681e9b1c93143c9218eadea52
bradboakes <129820955+bradboakes@users.noreply.github.com>
2025-01-17 13:56:43 +1300

    Update add-table-of-contents-to-basecamp.user.js

--- add-table-of-contents-to-basecamp.user.js
+++ add-table-of-contents-to-basecamp.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Add Table of Contents to Basecamp documents
 // @namespace    http://basecamp.com
-// @version      0.2
+// @version      0.3
 // @description  Add a table of contents at the top of Basecamp documents in a specific team
 // @author       Mathias Foster
 // @match        https://3.basecamp.com/*/buckets/*/documents/*
@@ -10,57 +10,63 @@
 // @run-at       document-idle
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
     console.log("Userscript is working");
 
     let scrape = document.querySelectorAll('.formatted_content > h1');
 
-    let header = document.querySelector('article > header');
+    // Target the main content instead of the header
+    let content = document.querySelector('article');
 
     let tableOfContents = document.createElement('section');
-    header.appendChild(tableOfContents);
+    tableOfContents.setAttribute('id', 'custom-table-of-contents'); // Add a unique ID
 
     let tableOfContentsUl = document.createElement('ul');
 
-    var css = '.menu-item:hover { text-decoration: underline; }',
+    // Scoped CSS for the Table of Contents
+    var css = '#custom-table-of-contents .menu-item:hover { text-decoration: underline; }',
         head = document.head || document.getElementsByTagName('head')[0],
         style = document.createElement('style');
     style.type = 'text/css';
     head.appendChild(style);
-    if (style.styleSheet){
-        // This is required for IE8 and below.
+    if (style.styleSheet) {
         style.styleSheet.cssText = css;
     } else {
         style.appendChild(document.createTextNode(css));
     }
 
-    for(var i in scrape) {
-        if(scrape[i].innerText && i > 0) {
-            // add id to heading
+    for (let i = 0; i < scrape.length; i++) {
+        if (scrape[i].innerText) {
+            // Add id to heading
             scrape[i].setAttribute("id", "heading" + i.toString());
 
-            // create new link
+            // Create new link
             let newA = document.createElement('a');
-            newA.setAttribute("onclick", "if(window.location.href.indexOf('#') !== -1) { var url = window.location.href; var hash = window.location.hash; var index_of_hash = url.indexOf(hash) || url.length; var hashless_url = url.substr(0, index_of_hash); window.location.href = hashless_url + '#heading" + i.toString() + "'; } else { window.location.href += '#heading" + i.toString() + "'; }");
-            newA.setAttribute("onmouseover", "");
-            newA.setAttribute("style", "cursor: pointer;");
             newA.classList.add("menu-item");
 
-            // create new list item
+            // Attach click event listener
+            newA.addEventListener("click", function () {
+                document.location.hash = "heading" + i.toString();
+            });
+
+            // Create new list item
             let newLi = document.createElement('li');
             newA.appendChild(newLi);
 
-            // create text inside list item
+            // Create text inside list item
             let newLiText = document.createTextNode(scrape[i].innerText);
             newLi.appendChild(newLiText);
 
-            // add to Table of Contents element
+            // Add to Table of Contents element
             tableOfContentsUl.appendChild(newA);
         }
     }
 
     tableOfContents.appendChild(tableOfContentsUl);
+
+    // Insert Table of Contents at the top of the main content
+    content.insertBefore(tableOfContents, content.firstChild);
 
     console.log(tableOfContents);
 })();
